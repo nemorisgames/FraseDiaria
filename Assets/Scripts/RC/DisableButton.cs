9034fa9f07d19b3cab840f276e949215c40ae679
njaramillo-ng <njaramillo@nemorisgames.com>
2017-11-15 12:52:02 -0300

    fix botones

---
+++ Assets/Scripts/RC/DisableButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableButton : MonoBehaviour {
+	UIButton button;
+
+	// Use this for initialization
+	void Awake(){
+		button = GetComponent<UIButton>();
+	}
+
+	public void toggleButton(){
+		button.enabled = !button.enabled;
+	}
+}
